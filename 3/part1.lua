9e991a60569e4c3d6b13605c1b6a87ed417b8dee
Vladimir Kazanov <vkazanov@inbox.ru>
2024-12-03 07:23:37 +0000

    3/part1

---
+++ 3/part1.lua
@@ -0,0 +1,24 @@
+local Part1 = {}
+
+function Part1.find(input)
+    local instructions = {}
+    for instr_str in input:gmatch("mul%(%d%d?%d?,%d%d?%d?%)") do
+        local left, right = string.match(instr_str, "mul%((%d%d?%d?),(%d%d?%d?)%)")
+        local instr = {
+            left = tonumber(left),
+            right = tonumber(right)
+        }
+        table.insert(instructions, instr)
+    end
+    return instructions
+end
+
+function Part1.run(instructions)
+    local num = 0
+    for _, instr in ipairs(instructions) do
+        num = num + instr.left * instr.right
+    end
+    return num
+end
+
+return Part1
