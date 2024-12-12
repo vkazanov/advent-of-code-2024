e0fc964eba5e87eaa0785ba35e8e3045a9a19407
Vladimir Kazanov <vkazanov@inbox.ru>
2024-12-12 15:27:05 +0000

    more optimisations

--- 12/part2.lua
+++ 12/part2.lua
@@ -11,65 +11,54 @@ local ssplit = aoc.str_split
 local arreq = aoc.arr_eq
 
 local function check(map, queue, acc)
-    map:print()
-
-    queue = queue or { pos { 1, 1 } }
+    queue = queue or { [pos { 1, 1 }] = true }
     acc = acc or 0
 
-    local first_region_pos = nil
-    while #queue > 0 do
-        local p = trem(queue)
-        if map[p] ~= "." then
-            first_region_pos = p
-            break
-        end
+    local first = next(queue)
+    while first and map[first] == "." do
+        queue[first] = nil
+        first = next(queue)
     end
-    if not first_region_pos then return acc end
+    if not first then return acc end
 
-    local region_area = 0
-    local region_queue = {[first_region_pos] = true}
-    local region_seen = {}
+    local area = 0
+    local region_queue = {[first] = true}
+    local seen = {}
+
+    local min_r, max_r = 0, map.dim.height
+    local min_c, max_c = 0, map.dim.width
 
     local this_pos = next(region_queue)
     while this_pos do
         region_queue[this_pos] = nil
-        region_seen[this_pos] = true
+        seen[this_pos] = true
 
         local this_plant = map[this_pos]
         assert(this_plant)
         assert(this_plant ~= ".")
         assert(this_pos)
 
-        region_area = region_area + 1
+
+        min_r = (this_pos.r < min_r) and this_pos.r or min_r
+        max_r = (this_pos.r > max_r) and this_pos.r or max_r
+        min_c = (this_pos.r < min_c) and this_pos.r or min_c
+        max_c = (this_pos.r > max_c) and this_pos.r or max_c
+
+        area = area + 1
 
         local next_poss = { this_pos:up(), this_pos:right(), this_pos:down(), this_pos:left() }
         for _, next_pos in ipairs(next_poss) do
             local next_plant
 
-            -- previously seen region posn
-            if region_seen[next_pos] then
+            -- seen region posn or not in the map
+            if seen[next_pos] or not map[next_pos] then
                 goto continue
             end
 
-            -- map end
-            if not map[next_pos] then
-                goto continue
-            end
-
+            -- same vs other region
             next_plant = map[next_pos]
-            -- same region
-            if next_plant == this_plant then
-                region_queue[next_pos] = true
-                goto continue
-            end
-
-            -- other region
-            if next_plant ~= this_plant then
-                if next_plant ~= "." then
-                    tins(queue, next_pos)
-                end
-                goto continue
-            end
+            if next_plant == this_plant then region_queue[next_pos] = true
+            else queue[next_pos] = true end
 
             ::continue::
         end
@@ -77,66 +66,60 @@ local function check(map, queue, acc)
         this_pos = next(region_queue)
     end
 
-    for k, _ in pairs(region_seen) do map[k] = "." end
+    for k, _ in pairs(seen) do map[k] = "." end
 
     local side_count = 0
 
-    local is_new_side = function(p, prev_is_side, opposite_to)
-        local in_region = region_seen[p]
+    local is_new_side = function(p, prev_is_side, get_opposite)
+        local in_region = seen[p]
         if not in_region then return false, false end
 
-        local is_side = not region_seen[opposite_to(p)]
+        local is_side = not seen[get_opposite(p)]
         if not is_side then return false, false end
         if prev_is_side then return true, false end
 
         return true, true
     end
 
-    -- check left vertical region sides
-    local opposite_to = function (p) return p:left() end
-    for c = 1, map.dim.width do
+    -- check left and right vertical region sides
+    local opposite_left = function (p) return p:left() end
+    local opposite_right = function (p) return p:right() end
+    for c = min_c, max_c do
         local prev_is_side = false
-        for r = 1, map.dim.height do
+        for r = min_r, max_r do
             local is_new
-            prev_is_side, is_new = is_new_side(pos { r, c }, prev_is_side, opposite_to)
+            prev_is_side, is_new = is_new_side(pos { r, c }, prev_is_side, opposite_right)
+            if is_new then side_count = side_count + 1 end
+        end
+
+        prev_is_side = false
+        for r = min_r, max_r do
+            local is_new
+            prev_is_side, is_new = is_new_side(pos { r, c }, prev_is_side, opposite_left)
             if is_new then side_count = side_count + 1 end
         end
     end
 
-    -- check right vertical region sides
-    opposite_to = function (p) return p:right() end
-    for c = 1, map.dim.width do
+    -- check up and down horizontal region sides
+    local opposite_up = function (p) return p:up() end
+    local opposite_down = function (p) return p:down() end
+    for r = min_r, max_r do
         local prev_is_side = false
-        for r = 1, map.dim.height do
+        for c = min_c, max_c do
             local is_new
-            prev_is_side, is_new = is_new_side(pos { r, c }, prev_is_side, opposite_to)
+            prev_is_side, is_new = is_new_side(pos { r, c }, prev_is_side, opposite_up)
+            if is_new then side_count = side_count + 1 end
+        end
+
+        prev_is_side = false
+        for c = min_c, max_c do
+            local is_new
+            prev_is_side, is_new = is_new_side(pos { r, c }, prev_is_side, opposite_down)
             if is_new then side_count = side_count + 1 end
         end
     end
 
-    -- check up horizontal region sides
-    opposite_to = function (p) return p:up() end
-    for r = 1, map.dim.height do
-        local prev_is_side = false
-        for c = 1, map.dim.width do
-            local is_new
-            prev_is_side, is_new = is_new_side(pos { r, c }, prev_is_side, opposite_to)
-            if is_new then side_count = side_count + 1 end
-        end
-    end
-
-    -- check down horizontal region sides
-    opposite_to = function (p) return p:down() end
-    for r = 1, map.dim.height do
-        local prev_is_side = false
-        for c = 1, map.dim.width do
-            local is_new
-            prev_is_side, is_new = is_new_side(pos { r, c }, prev_is_side, opposite_to)
-            if is_new then side_count = side_count + 1 end
-        end
-    end
-
-    return check(map, queue, acc + region_area * side_count)
+    return check(map, queue, acc + area * side_count)
 end
 
 aoc.PRINT = false
