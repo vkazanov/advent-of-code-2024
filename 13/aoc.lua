7bd90e2be15ac37563385ba3be4e695c2b5b3fe5
Vladimir Kazanov <vkazanov@inbox.ru>
2024-12-13 08:13:34 +0000

    13/part1

---
+++ 13/aoc.lua
@@ -0,0 +1,144 @@
+local aoc = {}
+
+aoc.tins = table.insert
+aoc.tcon = table.concat
+aoc.trem = table.remove
+aoc.tmov = table.move
+
+aoc.PRINT = true
+function aoc.mayprint(...)
+    if aoc.PRINT then
+        print(...)
+    end
+end
+
+function aoc.fline(fname)
+    fname = fname or "input.txt"
+    local f <close> = io.open(fname, "r")
+    return f:read("*all")
+end
+
+function aoc.flines(fname)
+    fname = fname or "input.txt"
+    local f = io.open(fname, "r")
+    return f:lines()
+end
+
+function aoc.str_split(str, conv, pattern)
+    if not conv then conv = tonumber; pattern = "%d+" end
+
+    local elements = {}
+    for e in str:gmatch(pattern) do aoc.tins(elements, conv(e)) end
+
+    return elements
+end
+
+function aoc.arr_eq(arr1, arr2)
+    if #arr1 ~= #arr2 then
+        return false
+    end
+
+    for i = 1, #arr1 do
+        if arr1[i] ~= arr2[i] then
+            return false
+        end
+    end
+
+    return true
+end
+
+function aoc.str_to_arr(str, conv_func)
+    if not conv_func then
+        conv_func = function(i) return i end
+    end
+    local chars = {}
+    for i = 1, #str do
+        local ch = str:sub(i, i)
+        chars[i] = conv_func and conv_func(ch) or ch
+    end
+    return chars
+end
+
+function aoc.str_to_intarr(str)
+    return aoc.str_to_arr(str, tonumber)
+end
+
+function aoc.foldl(f, acc, list)
+  for _, v in ipairs(list) do acc = f(acc, v) end
+  return acc
+end
+
+local poss = setmetatable({}, { __mode = "v" })
+local Pos = {}
+
+function aoc.pos(r, c)
+    if type(r) == "table" then
+        c = r[2]
+        r = r[1]
+    else
+        assert(type(r) == type(c))
+    end
+
+    local key = r .. "," .. c
+    local found = poss[key]
+    if found then return found end
+    local p = setmetatable({ r = r, c = c }, Pos)
+    poss[key] = p
+    Pos.__index = Pos
+    return p
+end
+
+function Pos.__add(left, right) return aoc.pos(left.r + right.r, left.c + right.c) end
+function Pos.__sub(left, right) return aoc.pos(left.r - right.r, left.c - right.c) end
+function Pos.__tostring(p) return p.r .. "," .. p.c end
+
+function Pos:up() return aoc.pos(self.r - 1, self.c) end
+function Pos:up_left() return aoc.pos(self.r - 1, self.c - 1) end
+function Pos:up_right() return aoc.pos(self.r - 1, self.c + 1) end
+function Pos:right() return aoc.pos(self.r, self.c + 1) end
+function Pos:down() return aoc.pos(self.r + 1, self.c) end
+function Pos:down_left() return aoc.pos(self.r + 1, self.c - 1) end
+function Pos:down_right() return aoc.pos(self.r + 1, self.c + 1) end
+function Pos:left() return aoc.pos(self.r,  self.c - 1) end
+
+local Map = {}
+
+function Map:print(conv_func)
+    if not aoc.PRINT then return end
+
+    conv_func = conv_func or function(ch) return ch end
+
+    for r2 = 1, self.dim.height do
+        for c2 = 1, self.dim.width do
+            local ch = self[aoc.pos(r2, c2)]
+            ch = conv_func(ch)
+            io.write(ch)
+        end
+        io.write("\n")
+    end
+end
+
+function aoc.mappify(lines, conv_func)
+    local map = setmetatable({}, Map)
+    Map.__index = Map
+
+    conv_func = conv_func or function(ch) return ch end
+
+    local w = 1
+    local r = 0
+
+    for line in lines do
+        r = r + 1
+        local c = 0
+        for ch in line:gmatch(".") do
+            c = c + 1
+            map[aoc.pos(r, c)] = conv_func(ch)
+        end
+        w = math.max(w, c)
+    end
+    map.dim = { width = w, height = r }
+
+    return map
+end
+
+return aoc
---
+++ 13/part2.lua
@@ -0,0 +1,189 @@
+local aoc = require "aoc"
+
+aoc.PRINT = false
+local mayprint = aoc.mayprint
+
+local pos = aoc.pos
+local tins = aoc.tins
+local tcon = aoc.tcon
+local trem = aoc.trem
+local ssplit = aoc.str_split
+local arreq = aoc.arr_eq
+
+local A_TOKS = 3
+local B_TOKS = 1
+local A_STEPS = 100
+local B_STEPS = 100
+local TOO_HIGH = A_TOKS * A_STEPS + B_TOKS * B_STEPS + 1 -- impossible price
+
+local function play(target, a_diff, b_diff,
+                    paid, cache, a_steps, b_steps)
+    if not paid then paid = 0 end
+    if not cache then cache = {} end
+    if not a_steps then a_steps = A_STEPS; b_steps = B_STEPS end
+
+    if target == pos { 0, 0 } then
+        mayprint("SOL: " .. a_steps .. "/" .. b_steps)
+        return paid
+    end
+    if target.r < 0 or target.c < 0 then return TOO_HIGH end
+    if a_steps <= 0 then return TOO_HIGH end
+    if b_steps <= 0 then return TOO_HIGH end
+
+    if cache[target] then return cache[target] end
+
+    local price_a = play(target - a_diff, a_diff, b_diff, paid + A_TOKS, cache, a_steps - 1, b_steps)
+    local price_b = play(target - b_diff, a_diff, b_diff, paid + B_TOKS, cache, a_steps, b_steps - 1)
+    local result = math.min(price_a, price_b)
+
+    cache[target] = result
+
+    return result
+end
+
+do
+    local a_diff = pos { 10, 10 }
+    local b_diff = pos { 11, 11 }
+    local target = pos { 3, 3 }
+
+    assert(play(target, a_diff, b_diff) == TOO_HIGH)
+end
+
+do
+    local a_diff = pos { 1, 1 }
+    local b_diff = pos { 100, 100 }
+    local target = pos { 3, 3 }
+
+    assert(play(target, a_diff, b_diff) == 9)
+end
+
+do
+    local a_diff = pos { 0, 1 }
+    local b_diff = pos { 0, 100 }
+    local target = pos { 0, 103 }
+
+    local price = play(target, a_diff, b_diff)
+    assert(price == 10, price)
+end
+
+do
+    aoc.PRINT = false
+
+    local a_diff = pos { 1, 0 }
+    local b_diff = pos { 100, 0 }
+    local target = pos { 103, 0 }
+
+    local price = play(target, a_diff, b_diff)
+    assert(price == 10, price)
+end
+
+do
+
+    local a_diff = pos { 1, 0 }
+    local b_diff = pos { 0, 1 }
+    local target = pos { 100, 0 }
+
+    local price = play(target, a_diff, b_diff)
+    assert(price ~= TOO_HIGH, "TOO HIGH")
+    assert(price == 100*A_TOKS, price)
+end
+
+do
+
+    local a_diff = pos { 1, 0 }
+    local b_diff = pos { 0, 1 }
+    local target = pos { 0, 100 }
+
+    local price = play(target, a_diff, b_diff)
+    assert(price ~= TOO_HIGH, "TOO HIGH")
+    assert(price == 100*B_TOKS, price)
+end
+
+do
+    local a_diff = pos { 1, 0 }
+    local b_diff = pos { 0, 3 }
+    local target = pos { 3, 99 }
+
+    local price = play(target, a_diff, b_diff)
+    assert(price ~= TOO_HIGH, "TOO HIGH")
+    assert(price == 3*A_TOKS + 33*B_TOKS, price)
+end
+
+do
+    aoc.PRINT = true
+
+    local a_diff = pos { 94, 34 }
+    local b_diff = pos { 22, 67 }
+    local target = pos { 8400, 5400 }
+
+    local price = play(target, a_diff, b_diff)
+    assert(price ~= TOO_HIGH, "TOO HIGH")
+    assert(price == 280, price)
+end
+
+do -- no sol
+-- Button A: X+26, Y+66
+-- Button B: X+67, Y+21
+-- Prize: X=12748, Y=12176
+    local a_diff = pos { 26, 66 }
+    local b_diff = pos { 67, 21 }
+    local target = pos { 12748, 12176 }
+
+    local price = play(target, a_diff, b_diff)
+    assert(price == TOO_HIGH, "NOT TOO HIGH")
+end
+
+do -- sol == 200
+-- Button A: X+17, Y+86
+-- Button B: X+84, Y+37
+-- Prize: X=7870, Y=6450
+
+    local a_diff = pos { 17, 86 }
+    local b_diff = pos { 84, 37 }
+    local target = pos { 7870, 6450 }
+
+    local price = play(target, a_diff, b_diff)
+    assert(price ~= TOO_HIGH, "TOO HIGH")
+    assert(price == 200, price)
+end
+
+do -- no sol
+-- Button A: X+69, Y+23
+-- Button B: X+27, Y+71
+-- Prize: X=18641, Y=10279
+    local a_diff = pos { 69, 23 }
+    local b_diff = pos { 27, 71 }
+    local target = pos { 18641, 10279 }
+
+    local price = play(target, a_diff, b_diff)
+    assert(price == TOO_HIGH, "NOT TOO HIGH")
+end
+
+do
+    aoc.PRINT = true
+
+    local total_min_price = 0
+    local lines = aoc.flines()
+    while true do
+        local line_a = lines()
+        local line_b = lines()
+        local line_target = lines()
+        assert(line_a and line_b and line_target)
+
+        local a_r, a_c = line_a:match("X%+(%d+)%s*,%s*Y%+(%d+)")
+        local a_diff = pos {tonumber(a_r), tonumber(a_c)}
+
+        local b_r, b_c = line_b:match("X%+(%d+)%s*,%s*Y%+(%d+)")
+        local b_diff = pos { tonumber(b_r), tonumber(b_c) }
+
+        local target_r, target_c = line_target:match("X=(%d+)%s*,%s*Y=(%d+)")
+        local target = pos { tonumber(target_r), tonumber(target_c) }
+
+        local price = play(target, a_diff, b_diff)
+        total_min_price = total_min_price + (price ~= TOO_HIGH and price or 0)
+
+        if not lines() then break end
+    end
+
+    print(total_min_price)
+end
