0e9120b75b11fb73e923f03753c1ce70a71a9f3b
Vladimir Kazanov <vkazanov@inbox.ru>
2024-12-13 15:27:54 +0000

    init day 14

---
+++ 14/part2.lua
@@ -0,0 +1,15 @@
+local aoc = require "aoc"
+
+aoc.PRINT = false
+local mayprint = aoc.mayprint
+
+local pos = aoc.pos
+local tins = aoc.tins
+local tcon = aoc.tcon
+local trem = aoc.trem
+local ssplit = aoc.str_split
+local arreq = aoc.arr_eq
+
+do
+    --
+end
