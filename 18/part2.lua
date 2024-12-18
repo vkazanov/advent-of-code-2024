2c3d233e2016f2695cb7373104fd7b6813b34646
Vladimir Kazanov <vkazanov@inbox.ru>
2024-12-18 06:05:49 +0000

    18/p1

---
+++ 18/part2.lua
@@ -0,0 +1,11 @@
+local aoc = require "aoc"
+aoc.PRINT = false
+local mayprint = aoc.mayprint
+
+local vec = aoc.vec
+local tins = aoc.tins
+local tcon = aoc.tcon
+local trem = aoc.trem
+local tunp = aoc.tunp
+local ssplit = aoc.str_split
+local arreq = aoc.arr_eq
