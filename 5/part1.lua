a10bc272038e6ed293725d2af5c9f7bf05cc649e
Vladimir Kazanov <vkazanov@inbox.ru>
2024-12-24 19:14:32 +0000

    5/1,2 - use sort

--- 5/part1.lua
+++ 5/part1.lua
@@ -1,90 +1,57 @@
 local tins = table.insert
 
-local function check(rules, update)
-    local rules_map = {}
-    for _, rule_str in pairs(rules) do
-        rules_map[rule_str] = true
-    end
+local function key(this, that)
+    return this .. "|" .. that
+end
 
+local function check(rules, update)
     for i, this in pairs(update) do
         for j, that in pairs(update) do
-            if i < j then
-                if rules_map[that .. "|" .. this] then
-                    return false
-                end
-            elseif i > j then
-                if rules_map[this .. "|" .. that] then
-                    return false
-                end
+            if i < j and rules[key(that, this)] then
+                return false
+            elseif i > j and rules[key(this, that)] then
+                return false
             end
         end
     end
 
-    return true, update[#update // 2 + 1]
+    return true
 end
 
 local function fix(rules, update)
-    local rules_map = {}
-    for _, rule_str in pairs(rules) do
-        rules_map[rule_str] = true
-    end
-
-    local fixed_update = {}
-    for _, v in pairs(update) do tins(fixed_update, v) end
-
-    for i, this in pairs(fixed_update) do
-        for j, that in pairs(fixed_update) do
-            if i < j then
-                if rules_map[that .. "|" .. this] then
-                    fixed_update[i], fixed_update[j] = that, this
-                    goto fixed
-                end
-            elseif i > j then
-                if rules_map[this .. "|" .. that] then
-                    fixed_update[j], fixed_update[i] = that, this
-                    goto fixed
-                end
-            end
+    table.sort(
+        update,
+        function(this, that)
+            return rules[key(this, that)]
         end
-    end
-
-    ::fixed::
-    return fixed_update
+    )
 end
 
-local rules = {}
 local file = io.open("input.txt", "r")
 
+local rules = {}
 for line in file:lines() do
     if line == "" then break end
-    tins(rules, line)
+    rules[line] = true
 end
-assert(#rules == 1176)
 
-local count_correct = 0
-local count_fixed = 0
+local correct_num = 0
+local fixed_num = 0
 for line in file:lines() do
     local update = {}
     for page in line:gmatch("%d+") do
         tins(update, tonumber(page))
     end
-    assert(#update % 2 == 1)
 
-    local is_correct, mid = check(rules, update)
-    if is_correct then
-        count_correct = count_correct + mid
+    if check(rules, update) then
+        correct_num = correct_num + update[#update // 2 + 1]
 
     else
-        local fixed_update = update
-        repeat
-            fixed_update = fix(rules, fixed_update)
-            is_correct, mid = check(rules, fixed_update)
-        until is_correct
-
-        count_fixed = count_fixed + mid
+        fix(rules, update)
+        fixed_num = fixed_num + update[#update // 2 + 1]
     end
 
 end
 
-assert(count_correct == 5248)
-assert(count_fixed == 4507)
+assert(correct_num == 5248)
+assert(fixed_num == 4507)
