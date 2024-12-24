25448ee027237470812c2c81c703f8123ce38f6a
Vladimir Kazanov <vkazanov@inbox.ru>
2024-12-24 18:28:51 +0000

    5/1,2 - descr

--- 5/part1.lua
+++ 5/part1.lua
@@ -1,6 +1,6 @@
+local tins = table.insert
 
 local function check(rules, update)
-    -- print(table.concat(update, ","))
     local rules_map = {}
     for _, rule_str in pairs(rules) do
         rules_map[rule_str] = true
@@ -30,7 +30,7 @@ local function fix(rules, update)
     end
 
     local fixed_update = {}
-    for _, v in pairs(update) do table.insert(fixed_update, v) end
+    for _, v in pairs(update) do tins(fixed_update, v) end
 
     for i, this in pairs(fixed_update) do
         for j, that in pairs(fixed_update) do
@@ -49,131 +49,44 @@ local function fix(rules, update)
             end
         end
     end
+
     ::fixed::
     return fixed_update
 end
 
-do
-    local rules = {
-        "47|53",
-        "97|13",
-        "97|61",
-        "97|47",
-        "75|29",
-        "61|13",
-        "75|53",
-        "29|13",
-        "97|29",
-        "53|29",
-        "61|53",
-        "97|53",
-        "61|29",
-        "47|13",
-        "75|47",
-        "97|75",
-        "47|61",
-        "75|61",
-        "47|29",
-        "75|13",
-        "53|13,"
-    }
+local rules = {}
+local file = io.open("input.txt", "r")
 
-    local is_correct, mid
-    is_correct, mid = check(rules, { 75, 47, 61, 53, 29 })
-    assert(is_correct)
-    assert(mid == 61)
-
-    is_correct, mid = check(rules, { 97,61,53,29,13 })
-    assert(is_correct)
-    assert(mid == 53)
-
-    is_correct, mid = check(rules, { 75,29,13 })
-    assert(is_correct)
-    assert(mid == 29)
-
-    -- incorrect ones
-
-    local update = { 75, 97, 47, 61, 53 }
-    print(table.concat(update, ","))
-    is_correct = check(rules, update)
-    assert(not is_correct)
-
-    local fixed_update = fix(rules, update)
-    print(table.concat(fixed_update, ","))
-    is_correct, mid = check(rules, fixed_update)
-    assert(is_correct)
-    assert(mid == 47)
-
-    update = { 61, 13, 29 }
-    print(table.concat(update, ","))
-    is_correct = check(rules, update)
-    assert(not is_correct)
-
-    fixed_update = fix(rules, update)
-    print(table.concat(fixed_update, ","))
-    is_correct, mid = check(rules, fixed_update)
-    assert(is_correct)
-    assert(mid == 29)
-
-    update = { 97, 13, 75, 29, 47 }
-    print(table.concat(update, ","))
-    is_correct = check(rules, update)
-    assert(not is_correct)
-
-    fixed_update = update
-    repeat
-        fixed_update = fix(rules, fixed_update)
-        print(table.concat(fixed_update, ","))
-        is_correct, mid = check(rules, fixed_update)
-    until is_correct
-    assert(is_correct)
-    assert(mid == 47)
+for line in file:lines() do
+    if line == "" then break end
+    tins(rules, line)
 end
+assert(#rules == 1176)
 
-do
-    local rules = {}
-    local file = io.open("input.txt", "r")
-    for line in file:lines() do
-        if line == "" then break end
-
-        table.insert(rules, line)
+local count_correct = 0
+local count_fixed = 0
+for line in file:lines() do
+    local update = {}
+    for page in line:gmatch("%d+") do
+        tins(update, tonumber(page))
     end
-    assert(#rules == 1176)
+    assert(#update % 2 == 1)
 
-    local num_correct, count_correct = 0, 0
-    local num_fixed, count_fixed = 0, 0
-    for line in file:lines() do
-        local update = {}
-        for page in line:gmatch("%d+") do
-            table.insert(update, tonumber(page))
-        end
-        assert(#update % 2 == 1)
+    local is_correct, mid = check(rules, update)
+    if is_correct then
+        count_correct = count_correct + mid
 
-        print("\nupdat: " .. table.concat(update, ","))
-        local is_correct, mid = check(rules, update)
-        if is_correct then
-            num_correct = num_correct + 1
-            count_correct = count_correct + mid
-            print("ok")
+    else
+        local fixed_update = update
+        repeat
+            fixed_update = fix(rules, fixed_update)
+            is_correct, mid = check(rules, fixed_update)
+        until is_correct
 
-        else
-            local fixed_update = update
-            repeat
-                fixed_update = fix(rules, fixed_update)
-                print("fixed: " .. table.concat(fixed_update, ","))
-                is_correct, mid = check(rules, fixed_update)
-            until is_correct
-            print("mid: " .. mid)
-
-            num_fixed = num_fixed + 1
-            count_fixed = count_fixed + mid
-            print("fixed")
-        end
-
+        count_fixed = count_fixed + mid
     end
 
-    assert(count_correct == 5248)
-    assert(count_correct == 4507)
-
-    file:close()
 end
+
+assert(count_correct == 5248)
+assert(count_fixed == 4507)
