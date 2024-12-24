5d5a9befa5600fa07002d927b8e7bcb595757fa7
Vladimir Kazanov <vkazanov@inbox.ru>
2024-12-24 11:27:27 +0000

    1/1,2 description

--- 1/part1.lua
+++ 1/part1.lua
@@ -12,26 +12,20 @@ for line in file:lines() do
    assert(left_id and right_id)
    left_id = tonumber(left_id)
    right_id = tonumber(right_id)
-   print(left_id, "   ", right_id)
    table.insert(left_arr, left_id)
    table.insert(right_arr, right_id)
 end
 
 file:close()
 
-print("Left = ", #left_arr, "Right = ", #right_arr)
 assert(#left_arr == #right_arr)
 
 table.sort(left_arr)
 table.sort(right_arr)
 
-for i, value in ipairs(left_arr) do
-    print(left_arr[i], "   ", right_arr[i])
-end
-
 local total_diff = 0
-for i = 1, #left_arr, 1 do
+for i = 1, #left_arr do
    total_diff = total_diff + math.abs(left_arr[i] - right_arr[i])
 end
 
-print("Total diff = ", total_diff)
+assert(total_diff == 2057374)
