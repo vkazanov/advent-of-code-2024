f9c5996e2a2f1f19985724f65197c2c032a1f7f1
Vladimir Kazanov <vkazanov@inbox.ru>
2024-12-25 14:40:09 +0000

    7/1,2 - descr

--- 7/part1.lua
+++ 7/part1.lua
@@ -1,74 +1,39 @@
-local DOPRINT = false
-
 local function check(sum, numbers, i)
-    -- TODO: check that division is correct
-    -- TODO: check the base case
     if i == nil then i = #numbers end
-    if DOPRINT then print(sum .. ": " .. table.concat(numbers, ",") .. "[" .. i .. "]") end
 
     local number = numbers[i]
     if i == 1 then
-        if sum == number then
-            if DOPRINT then print(i .. "done") end
-            return true
-        end
-
-        if sum - number == 0 then
-            if DOPRINT then print(i .. "done") end
-            return true
-        end
-
+        if sum == number then return true end
         return false
     end
 
-    local next_i = i - 1
-    -- check the division case
-    if sum % number == 0 and check(sum // number, numbers, next_i) then
-        if DOPRINT then print(i .. "div") end
+    if sum % number == 0 and check(sum // number, numbers, i - 1) then
         return true
     end
 
-    if check(sum - number, numbers, next_i) then
-        if DOPRINT then print(i .. "add") end
+    if check(sum - number, numbers, i - 1) then
         return true
     end
 
     return false
 end
 
-
-do
-    assert(check(190, { 10, 19 }))
-    assert(check(3267, { 81, 40, 27 }))
-    assert(not check(83, { 17, 5 }))
-    assert(not check(156, { 15, 6 }))
-    assert(not check(7290, { 6, 8, 6, 15 }))
-    assert(not check(161011, { 16, 10, 13 }))
-    assert(not check(192, { 17, 8, 14 }))
-    assert(not check(21037, { 9, 7, 18, 13 }))
-    assert(check(292, { 11, 6, 16, 20 }))
-end
-
 do
     local input = {}
     local file = io.open("input.txt", "r")
     for line in file:lines() do
-        -- print(line)
         local sum, num_list = line:match("(%d+): ([%d%s]+)")
         local numbers = {}
         for num in num_list:gmatch("%d+") do
             table.insert(numbers, tonumber(num))
         end
-        print(sum .. ": " .. table.concat(numbers, ","))
-        input[sum] = numbers
+        input[tonumber(sum)] = numbers
     end
-    file:close()
 
     local result = 0
     for sum, numbers in pairs(input) do
-        if check(sum, numbers) then
-            result = result + sum
-        end
+        if check(sum, numbers) then result = result + sum end
     end
-    print(result)
+
+    assert(result == 5702958180383)
 end
--- 7/part2.lua
+++ 7/part2.lua
@@ -1,107 +1,33 @@
-local DOPRINT = false
--- local DOPRINT = true
+local tins = table.insert
+local trem = table.remove
 
 local function add(left, right) return left + right end
 local function mul(left, right) return left * right end
 local function con(left, right) return tonumber(tostring(left) .. tostring(right)) end
 
-local OP_TO_FUN = {
-    ["+"] = add,
-    ["*"] = mul,
-    ["|"] = con,
-}
+local OPS = {add,mul,con,}
 
-local function calc(sum, numbers, operators)
+local function calc(numbers, operators)
     assert(#numbers == #operators + 1)
 
-    if DOPRINT then print("NUM: " .. table.concat(numbers, ",")) end
-    if DOPRINT then print("OPS: " .. table.concat(operators, ",")) end
-
-    local op_stack, num_stack = {}, {}
-    for i = #operators, 1, -1 do table.insert(op_stack, operators[i]) end
-    for i = #numbers, 1, -1 do table.insert(num_stack, numbers[i]) end
-
-    if DOPRINT then print("COPS: " .. table.concat(op_stack, ",")) end
-    if DOPRINT then print("CNUM: " .. table.concat(num_stack, ",")) end
-
-    while #op_stack > 0 do
-        local op = table.remove(op_stack)
-        local right = table.remove(num_stack)
-        local left = table.remove(num_stack)
-        table.insert(num_stack, OP_TO_FUN[op](right, left))
+    local left = numbers[1]
+    for i, op in ipairs(operators) do
+        left = op(left, numbers[i + 1])
     end
-    assert(#num_stack == 1)
 
-    if DOPRINT then print("SUM: " .. sum) end
-    if DOPRINT then print("ACC: " .. num_stack[1]) end
-    if DOPRINT then print("RES: " .. tostring(num_stack[1] == sum)) end
-
-    return num_stack[1] == sum
+    return left
 end
 
--- TODO: possible to skin branches if doing calc on the fly and pruning when the running
--- sum is too big to be viable
-local function check(sum, numbers, i, operators)
-    if DOPRINT then print(sum .. ": " .. table.concat(numbers, ",") .. "[" .. tostring(i) .. "]") end
+local function check(sum, numbers, operators)
+    if #operators + 1 == #numbers then return calc(numbers, operators) == sum end
 
-    if i == nil then i = 1; operators = {} end
-
-    if i == #numbers then
-        return calc(sum, numbers, operators)
-    else
-        table.insert(operators, "+")
-        if check(sum, numbers, i + 1, operators) then return true end
-        table.remove(operators)
-
-        table.insert(operators, "*")
-        if check(sum, numbers, i + 1, operators) then return true end
-        table.remove(operators)
-
-        table.insert(operators, "|")
-        if check(sum, numbers, i + 1, operators) then return true end
-        table.remove(operators)
+    for _, op in pairs(OPS) do
+        tins(operators, op)
+        if check(sum, numbers, operators) then return true end
+        trem(operators)
     end
 
     return false
-end
-
-do
-    -- assert(calc(6, {1, 2, 3}, {"+", "+"}))
-    -- assert(calc(5, {1, 2, 3}, {"*", "+"}))
-    -- assert(calc(12, {1, 2}, {"|"}))
-    -- assert(calc(223, {1, 1, 2, 3}, {"+", "|", "|"}))
-    -- assert(calc(7290, {6, 8, 6, 15}, {"*", "|", "*"}))
-    -- assert(calc(15156126976,
-    --             { 6, 3, 70, 9, 9, 3, 8, 4, 1, 9, 973, 3 },
-    --             {"*","|","*","+","|","+","|","*","*","|","+"}))
-end
-
-do
-    -- assert(check(11, { 1, 1 }))
-    -- assert(not check(12, { 1, 10}))
-    -- assert(check(10, { 1, 10}))
-    -- assert(check(1, { 1 }))
-
-    -- assert(check(10, { 1, 10}))
-    -- assert(check(10, { 1, 0}))
-    -- assert(check(10, { 9, 1}))
-    -- assert(not check(21, { 9, 1}))
-
-    -- assert(check(190, { 10, 19 }))
-    -- assert(check(3267, { 81, 40, 27 }))
-    -- assert(not check(83, { 17, 5 }))
-    -- assert(check(156, { 15, 6 }))
-
-    -- assert(check(7290, { 6, 8, 6, 15 }))
-
-    -- assert(not check(161011, { 16, 10, 13 }))
-    -- assert(check(192, { 17, 8, 14 }))
-    -- assert(not check(21037, { 9, 7, 18, 13 }))
-    -- assert(check(292, { 11, 6, 16, 20 }))
-
-    -- print("test")
-    -- assert( check(15156126976, { 6, 3, 70, 9, 9, 3, 8, 4, 1, 9, 973, 3 }) )
-    -- assert( check(56083790, { 93, 7, 89, 932, 67, 58, 5 }) )
 end
 
 do
@@ -111,17 +37,14 @@ do
         local sum, num_list = line:match("(%d+): ([%d%s]+)")
         local numbers = {}
         for num in num_list:gmatch("%d+") do
-            table.insert(numbers, tonumber(num))
+            tins(numbers, tonumber(num))
         end
         input[tonumber(sum)] = numbers
     end
-    file:close()
 
     local result = 0
     for sum, numbers in pairs(input) do
-        if check(sum, numbers) then
-            result = result + sum
-        end
+        if check(sum, numbers, {}) then result = result + sum end
     end
-    print(result)
+    assert(result == 92612386119138)
 end
