817a68f6d37e8a02418346ce1c3bb5846bb772a1
Vladimir Kazanov <vkazanov@inbox.ru>
2025-01-02 19:04:09 +0000

    22/2 optimisations

--- 22/part2.lua
+++ 22/part2.lua
@@ -1,59 +1,41 @@
 local aoc = require "aoc"
-local tcon = table.concat
 local deque = require "deque"
 
-local function mix(num, secret)
-    return num ~ secret
-end
-
-local function prune(secret)
-    return secret % 16777216
-end
-
+local function mix(num, secret) return num ~ secret end
+local function prune(secret) return secret % 16777216 end
 local function next_secret(secret)
-    local a = secret << 6
-    secret = prune(mix(a, secret))
-
-    local b = secret >> 5
-    secret = prune(mix(b, secret))
-
-    local c = secret << 11
-    secret = prune(mix(c, secret))
-
+    secret = prune(mix(secret << 6, secret))
+    secret = prune(mix(secret >> 5, secret))
+    secret = prune(mix(secret << 11, secret))
     return secret, secret % 10
 end
 
-local global_seq2count = {}
+local seq2count = {}
 for l in aoc.flines() do
 
     local seq = deque.new()
-
-    local seq2count = {}
+    local seq2seen = {}
 
     local secret = tonumber(l)
     local price = secret % 10
     for _ = 1, 2000 do
         local new_secret, new_price = next_secret(secret)
-        local new_diff = new_price - price
-
-        seq:push_right(new_diff)
-        if seq:length() > 4 then seq:pop_left() end
-
-        if seq:length() == 4 then
-            local key = tcon(seq:contents(), ",")
-            if not seq2count[key] then seq2count[key] = new_price end
+        seq.tail = seq.tail + 1
+        seq[seq.tail] = new_price - price
+        local tail, head = seq.tail, seq.head
+        if tail - head >= 4 then
+            local key = seq[tail] + 100*seq[tail - 1] + 10000*seq[tail - 2] + 1000000*seq[tail - 3]
+            if not seq2seen[key] then
+                seq2seen[key] = true
+                seq2count[key] = (seq2count[key] or 0) + new_price
+            end
         end
-
         secret, price = new_secret, new_price
-    end
-
-    for key, count in pairs(seq2count) do
-        global_seq2count[key] = (global_seq2count[key] or 0) + count
     end
 end
 
 local max_count = 0
-for _, count in pairs(global_seq2count) do
+for _, count in pairs(seq2count) do
     if count > max_count then max_count = count end
 end
 
