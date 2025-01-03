cb692e2b162b756e2bb88130f501726a78a04959
Vladimir Kazanov <vkazanov@inbox.ru>
2025-01-03 09:48:39 +0000

    23/2

--- 23/part2.lua
+++ 23/part2.lua
@@ -1,5 +1,6 @@
 local aoc = require "aoc"
 
+local tcon = table.concat
 local Set = aoc.Set
 
 local computers = Set()
@@ -14,27 +15,28 @@ for line in aoc.flines() do
     computers:add(r)
 end
 
-local function find_cliques(r, p, x, report)
-    if r:size() == 3 then
-        for c in pairs(r) do
-            if c:sub(1, 1) == "t" then
-                report(r)
-                break
-            end
-        end
+local function find_max_cliques(r, p, x, report)
+    if p:is_empty() and x:is_empty() then
+        report(r)
     end
 
     for v in pairs(p) do
         local conn_vs = conns[v]
-        find_cliques(r:union(Set { v }), p:intersect(conn_vs), x:intersect(conn_vs),
+        find_max_cliques(r:union(Set { v }), p:intersect(conn_vs), x:intersect(conn_vs),
                      report)
         p = p:diff(Set { v })
         x = x:union(Set { v })
     end
 end
 
-local counter = 0
-local function count_cliques(c) counter = counter + 1 end
-find_cliques(Set(), computers, Set(), count_cliques)
+local max_c = Set()
+local function find_largest(c)
+    if c:size() > max_c:size() then
+        max_c = c
+    end
+end
+find_max_cliques(Set(), computers, Set(), find_largest)
+local comps = max_c:members()
+table.sort(comps)
 
-assert(counter, 1077, counter)
+assert(tcon(comps, ",") == "bc,bf,do,dw,dx,ll,ol,qd,sc,ua,xc,yu,zt")
